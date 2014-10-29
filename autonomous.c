aca0b0c99668501872c5a71c19ad86b70afc1030
CosineP <judahiii@gmail.com>
2014-10-29 15:38:08 -0400

    Add moveMotorTurns function for autonomous mode
    This is a revert essentially from "Ready to test with the Chassis" with
    that function. I also added a comment!

--- autonomous.c
+++ autonomous.c
@@ -14,6 +14,7 @@
 // Global variables
 int ir[6];
 
+// Check whether we arrived at the IR beacon
 bool hasArrived()
 {
 	const int arrivedThreshold = 105;
@@ -22,6 +23,18 @@ bool hasArrived()
 		if (ir[i] > arrivedThreshold) return true;
 	}
 	return false;
+}
+
+// moveMotorTurns(backLeft, -100, 4.5); Will turn motor backwards 4 times and 180 degrees after that
+// Warning: will delete encoder value, should backup if you will need it later
+void moveMotorTurns(int motorType, int speed, float turns)
+{
+	int originalSpeed = motor[motorType];
+	motor[motorType] = speed;
+	nMotorEncoder[motorType] = 0;
+	int encoderValue = turns * 1440; // Encoder value per rotation
+	while (abs(nMotorEncoder[motorType]) < encoderValue)	{}
+	motor[motorType] = 0;
 }
 
 task main()
