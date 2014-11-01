5e3faba97032e7a39cdfb68415e74a4f7df5f521
CosineP <judahiii@gmail.com>
2014-11-01 16:02:55 -0400

    Sync the motors
    I LOVE TO SYNC. NOW I'LL SYNC THE CODE.

--- library.c
+++ library.c
@@ -2,40 +2,47 @@
 
 #include "JoystickDriver.c"
 
-void setLeft(int speed)
+// Library variables
+
+const float spoolCircumference = 6;
+
+const int liftAutoSpeed = 75;
+
+const int amountAboveTubes = 5;
+
+// Library functions
+
+void syncLift(void)
 {
-	motor[leftBack] = speed;
-	motor[leftFront] = speed;
+	int speed = motor[liftLeft];
+	if (speed != 0)
+	{
+		while (nMotorEncoder[liftLeft] != nMotorEncoder[liftRight])
+		{
+			// We're slightly lopsided, better fix this
+			bool leftAboveRight = nMotorEncoder[liftLeft] > nMotorEncoder[liftRight];
+			motor[liftLeft] = !leftAboveRight * speed;
+			motor[liftRight] = leftAboveRight * speed;
+		}
+		motor[liftLeft] = speed;
+		motor[liftRight] = speed;
+	}
 }
 
-void setRight(int speed)
+void setLift(int velocity)
 {
-	motor[rightBack] = speed;
-	motor[rightFront] = speed;
+	motor[liftLeft] = velocity;
+	motor[liftRight] = velocity;
+	syncLift();
 }
 
-void setLift(int speed)
+void zeroEncoders(void)
 {
-	motor[liftLeft] = speed;
-	motor[liftRight] = speed;
+	nMotorEncoder[liftLeft] = 0;
+	nMotorEncoder[liftRight] = 0;
+	nMotorEncoder[left] = 0;
+	nMotorEncoder[right] = 0;
 }
 
-// moveMotorTurns(backLeft, 100, -4.5); Will turn motor backwards 4 times and 180 degrees after that
-// Warning: will delete encoder value, should backup if you will need it later
-void moveMotorTurns(int motorType, int speed, float turns)
-{
-	int originalSpeed = motor[motorType];
-	motor[motorType] = speed * (turns < 0 ? -1 : 1);
-	nMotorEncoder[motorType] = 0;
-	int encoderValue = abs(turns) * 1440; // Encoder value per rotation
-	while (abs(nMotorEncoder[motorType]) < encoderValue)	{}
-	motor[motorType] = 0;
-}
-
-void liftHeight(int height)
-{
-	// All Measurements in centimeters
-	// Todo: Warning: Circumference is subject to change
-	const int spoolCircumference = 6;
-	moveMotorTurns(liftLeft, 50, height/spoolCircumference);
-}
+int turnsToEncoders(float turns) { return turns * 1440; }
+float encodersToTurns(int encoders) { return encoders / 1440.0; }
